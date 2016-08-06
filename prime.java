506caa23512138e1e6feda89cfcaf4db0b63232e
vaanparanth <kspv1k95@gmail.com>
2016-08-06 17:57:46 +0530

    Create prime.java

---
+++ prime.java
@@ -0,0 +1,13 @@
+public class prime{
+public static void main(String ar[]){
+int a[]=new int[100];
+if(a[]%2==0)
+{
+system.out.println("prime");
+int n[]=n[]+a[];
+}
+else
+{
+system.ou.println("not prime");}
+System.out.println(n);
+}}
