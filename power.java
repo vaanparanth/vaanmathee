08f79d0c36296a5317f2c148556b81fc9d52e3a4
vaanparanth <kspv1k95@gmail.com>
2016-08-06 17:07:31 +0530

    Create power.java

---
+++ power.java
@@ -0,0 +1,19 @@
+public class power{
+public static void main(String args[])
+{
+int num=12345;
+int sum=0;
+int count;
+num1=num;
+while(num!=0)
+{
+int a=num/10;
+count++;
+}
+int b;
+for(int i=0;i<count;i++)
+{
+b=num%10;
+num=num/10;
+sum=(sum*10)+b;}
+system.out.println(sum);}}
