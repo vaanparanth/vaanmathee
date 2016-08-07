0142457eadd19d6209b7db96601f216cfe4898c8
vaanparanth <kspv1k95@gmail.com>
2016-08-07 10:06:47 +0530

    Create public .java

---
+++ public .java
@@ -0,0 +1,18 @@
+class public{
+public static void main(String args[]){
+String str="vaanmathee";
+String Buffer bf=new StringBuffer(Str);
+for(int i=0;i<buf.length;i++){
+fir(int j=i+1;i<buf.length();j++)
+{
+if(buf.charAt(i)==buf.charAt(j))
+{
+count++;
+buf.deletecharAt(j);
+y--;
+}}a[i]==count;
+System.out.prinltn(buf);
+for(int i=0;i<a.length;i++)
+{
+q=q+(a[i]%2);}
+}
