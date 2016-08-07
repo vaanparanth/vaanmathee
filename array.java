09b8fe1709fa5e7f92734ad0396d8b80371b3c79
vaanparanth <kspv1k95@gmail.com>
2016-08-07 10:14:16 +0530

    Create array.java

---
+++ array.java
@@ -0,0 +1,10 @@
+class array{
+public static void main(String ar[])
+{
+int a[]=new int[50];
+if(a.length()>a.length()-1)
+{
+System.out.println("a is greater");}
+else
+System.out.println("b is greater");
+}
