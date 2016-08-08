1ced8ceb90fa20fe1df673ec6cf09fcfc77fbebc
vaanparanth <kspv1k95@gmail.com>
2016-08-08 11:32:57 +0530

    Create maxmin.java

---
+++ maxmin.java
@@ -0,0 +1,11 @@
+public maxmin
+{
+public static void main(String ar[]){
+int a=10;
+int b=20;
+if(a>b){
+System.out.println("a isthe larger value")}
+else
+{
+System.out.println("b is larger value");
+}}}
